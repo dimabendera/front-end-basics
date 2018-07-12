32f53b89ad74577ef27117061e118cb212200fba
Pronachay Dmitry <pronachaydmitry@localhost.localdomain>
2018-07-12 16:10:24 +0300

    add local Storage

--- static/index.js
+++ static/index.js
@@ -10,33 +10,40 @@ class BooksList{
         this._BooksList = document.querySelector("body > .content");
     }
     createBook(key, author, name, img, isRight){
-        return '<div class="item" id="item'+key+'"><div class="pic"><span><img src="'+img+'"></span></div><div class="title"><span><b>Название:</b> "'+name+'"</span><span><b>Автор:</b> '+author+'</span></div><div class="'+(isRight?"before":"after")+'"></div></div>';
+        return '<div class="item" id="'+key+'"><div class="pic"><span><img src="'+img+'"></span></div><div class="title"><span><b>Название:</b> "'+name+'"</span><span><b>Автор:</b> '+author+'</span></div><div class="'+(isRight?"before":"after")+'"></div></div>';
     };
     async loadBooks(){
-        if(1){
+        if(!localStorage.length){
             await connectToJSON((res)=>{
                 for(let key in res){
+                    res[key].isRight=0;
                     this._BooksList.querySelector(".left")
-                    .insertAdjacentHTML("beforeEnd", this.createBook(key, res[key].author, res[key].name, res[key].img, 0));
+                    .insertAdjacentHTML("beforeEnd", this.createBook("item"+key, res[key].author, res[key].name, res[key].img, 0));
                 }
-                
             });
-        }else{
-    
+        }else{ 
+            this._BooksList.querySelector(".left").innerHTML=localStorage.getItem("left");
+            this._BooksList.querySelector(".right").innerHTML=localStorage.getItem("right");
         }
         this.giveOnClickEvents();
     };
+    saveToLocalStorage(){
+        localStorage.setItem("left", this._BooksList.querySelector(".left").innerHTML);
+        localStorage.setItem("right", this._BooksList.querySelector(".right").innerHTML);
+    };
     giveOnClickEvents(){
-        let rootNode = this._BooksList;
+        let root = this;
         function goRight(ev){
             ev.currentTarget.onclick = goLeft;
             ev.currentTarget.className = "before";
-            rootNode.querySelector(".right").appendChild(ev.currentTarget.parentNode)
+            root._BooksList.querySelector(".right").appendChild(ev.currentTarget.parentNode);
+            root.saveToLocalStorage();
         };
         function goLeft(ev){
             ev.currentTarget.onclick = goRight;
             ev.currentTarget.className = "after";
-            rootNode.querySelector(".left").appendChild(ev.currentTarget.parentNode)
+            root._BooksList.querySelector(".left").appendChild(ev.currentTarget.parentNode);
+            root.saveToLocalStorage();
         };
         let elems = document.querySelectorAll('div.item>div.after');
         for(let i=0;i<elems.length;i++){
@@ -48,7 +55,7 @@ class BooksList{
         }
     };
 };
-window.onload = function (){ 
+window.onload = ()=>{ 
     let list = new BooksList();
     list.loadBooks();
     //BooksList.goRight(1);
