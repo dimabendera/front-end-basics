190507153e5930a6d67f474a4d94f90cbacf178f
Pronachay Dmitry <pronachaydmitry@localhost.localdomain>
2018-07-12 17:47:45 +0300

    reconstruct code

--- static/index.js
+++ static/index.js
@@ -7,42 +7,54 @@ function connectToJSON(callback){
 // конструктор
 class BooksList{
     constructor(){
-        this._BooksList = document.querySelector("body > .content");
+        this._BooksHTML = document.querySelector("body > .content");
+        this._Books = {};
     }
-    createBook(key, author, name, img, isRight){
-        return '<div class="item" id="'+key+'"><div class="pic"><span><img src="'+img+'"></span></div><div class="title"><span><b>Название:</b> "'+name+'"</span><span><b>Автор:</b> '+author+'</span></div><div class="'+(isRight?"before":"after")+'"></div></div>';
-    };
-    async loadBooks(){
-        if(!localStorage.length){
+    async init(){
+        if(!localStorage.getItem("books")){
             await connectToJSON((res)=>{
-                for(let key in res){
-                    res[key].isRight=0;
-                    this._BooksList.querySelector(".left")
-                    .insertAdjacentHTML("beforeEnd", this.createBook("item"+key, res[key].author, res[key].name, res[key].img, 0));
-                }
+                this._Books = res;
             });
         }else{ 
-            this._BooksList.querySelector(".left").innerHTML=localStorage.getItem("left");
-            this._BooksList.querySelector(".right").innerHTML=localStorage.getItem("right");
+            let books = JSON.parse(localStorage.getItem("books"));
+            this._Books=(books===null)?{}:books;
         }
-        this.giveOnClickEvents();
+    }
+    createHTMLBook(key, author, name, img, isRight){
+        return '<div class="item" id="'+key+'"><div class="pic"><span><img src="'+img+'"></span></div><div class="title"><span><b>Название:</b> "'+name+'"</span><span><b>Автор:</b> '+author+'</span></div><div class="'+(isRight?"before":"after")+'"></div></div>';
+    };
+    loadHTML(regFilter){
+        for(let key in this._Books){
+            if(regFilter instanceof RegExp){
+                if(!regFilter.test(this._Books[key].author)){
+                    continue;
+                }
+            }
+            if(this._Books[key].isRight){
+                this._BooksHTML.querySelector(".right").insertAdjacentHTML("beforeEnd", this.createHTMLBook(key, this._Books[key].author, this._Books[key].name, this._Books[key].img, 1));
+            }else{
+                this._BooksHTML.querySelector(".left").insertAdjacentHTML("beforeEnd", this.createHTMLBook(key, this._Books[key].author, this._Books[key].name, this._Books[key].img, 0));
+            }
+        }
+        this.initOnClickEvents();
     };
     saveToLocalStorage(){
-        localStorage.setItem("left", this._BooksList.querySelector(".left").innerHTML);
-        localStorage.setItem("right", this._BooksList.querySelector(".right").innerHTML);
+        localStorage.setItem("books", JSON.stringify(this._Books));
     };
-    giveOnClickEvents(){
+    initOnClickEvents(){
         let root = this;
         function goRight(ev){
             ev.currentTarget.onclick = goLeft;
             ev.currentTarget.className = "before";
-            root._BooksList.querySelector(".right").appendChild(ev.currentTarget.parentNode);
+            root._Books[ev.currentTarget.parentNode.getAttribute('id')].isRight=1;
+            root._BooksHTML.querySelector(".right").appendChild(ev.currentTarget.parentNode);
             root.saveToLocalStorage();
         };
         function goLeft(ev){
             ev.currentTarget.onclick = goRight;
             ev.currentTarget.className = "after";
-            root._BooksList.querySelector(".left").appendChild(ev.currentTarget.parentNode);
+            root._Books[ev.currentTarget.parentNode.getAttribute('id')].isRight=0;
+            root._BooksHTML.querySelector(".left").appendChild(ev.currentTarget.parentNode);
             root.saveToLocalStorage();
         };
         let elems = document.querySelectorAll('div.item>div.after');
@@ -57,6 +69,6 @@ class BooksList{
 };
 window.onload = ()=>{ 
     let list = new BooksList();
-    list.loadBooks();
-    //BooksList.goRight(1);
+    list.init();
+    list.loadHTML();
 }
